4d5617795324239a4c5d53e819ad9f8bae30c190
Lawrence Schmid <lawrence.schmid@gmail.com>
2024-05-29 07:49:12 +0100

    Removed project

--- app/src/main/java/com/citex/twelve_step_recovery/exceptions/ResourceUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.citex.twelve_step_recovery.exceptions;
-
-public class ResourceUnavailableException extends Exception {
-    public ResourceUnavailableException() {
-        super();
-    }
-}
