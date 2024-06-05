3b67f660306a3273523c8587369508fd6b10b8a6
Lawrence Schmid <lawrence.schmid@gmail.com>
2024-06-05 15:15:32 +0100

    Click on website link works on splash screen

--- 12-Step Recovery/app/src/main/java/com/citex/twelve_step_recovery/SplashScreenActivity.java
+++ 12-Step Recovery/app/src/main/java/com/citex/twelve_step_recovery/SplashScreenActivity.java
@@ -3,10 +3,20 @@ package com.citex.twelve_step_recovery;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends Activity {
+
+    static Boolean websiteClicked = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,14 +24,33 @@ public class SplashScreenActivity extends Activity {
 
         // Show the splash screen.
         setContentView(R.layout.activity_splash_screen);
+        TextView splashWebsite = findViewById(R.id.text_splash_website);
+        splashWebsite.setMovementMethod(LinkMovementMethod.getInstance());
+        splashWebsite.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                Uri uri = Uri.parse("http://www.recoverymeetingfinder.com" );
+                Intent intent = new Intent( Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                websiteClicked = true;
+            }
+        } );
+
+        // Start application after displaying splash screen.
         new Thread(() -> {
             try {
                 Thread.sleep(1500);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            startApp();
-            finish();
+
+            if(!websiteClicked) {
+                startApp();
+                finish();
+            }
+
         }).start();
     }
 
